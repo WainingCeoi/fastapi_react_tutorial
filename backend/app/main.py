763640e1633cd60c41f4a75bb2c05ec6e314f4e5
Waining Ceoi <weiningxu1018@gmail.com>
2026-07-09 13:37:08 +0800

    Step 3: React page fetching and rendering contacts

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:5173"],
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 
 class Contact(BaseModel):
     id: int
@@ -10,8 +16,9 @@ class Contact(BaseModel):
     email: str
 
 contacts = [
-    {"id": 1, "name": "John Doe", "email": "john.doe@example.com"},
-    {"id": 2, "name": "Jane Smith", "email": "jane.smith@example.com"}
+    {"id": 1, "name": "Walter White", "email": "walter.white@example.com"},
+    {"id": 2, "name": "Jesse Pinkman", "email": "jesse.pinkman@example.com"},
+    {"id": 3, "name": "Saul Goodman", "email": "saul.goodman@example.com"},
 ]
 
 @app.get("/")
